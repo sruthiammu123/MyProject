0c3931fc0915655fedbaa2bbc3fa7f50eb23733b
Sruthi Sivan K <sruthiammus98@gmail.com>
2020-12-31 23:43:22 +0530

    version 2

---
+++ todo/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p> Vuetify is a powerful material design based on vue with beautifully Handcrafted Material Component.</p>
+    <p> Vuetify is a powerful material design based on vue with beautifully Handcrafted Material Component.</p>
+    <p> Vuetify is a powerful material design based on vue with beautifully Handcrafted Material Component.</p>
+  </div>
+</template>
